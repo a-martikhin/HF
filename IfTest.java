ee1b7eff96b97722639c6f24b4a04bc08c2da5ab
a.martikhin <a.martikhin@mail.ru>
2022-07-23 01:21:12 +0300

    Повторно навел порядок

--- IfTest.java
+++
@@ -1,9 +0,0 @@
-public class IfTest{
-	public static void main(String[] args){
-	    int i = 3;
-        if(i == 3){
-			System.out.println("Х должен равняться 3");
-		}		
-		System.out.println("Эта строка выполняется в любом случае");
-	}
-}
--- MyFirstApp.java
+++
@@ -1,6 +0,0 @@
-public class MyFirstApp{
-	public static void main(String[] args){
-		System.out.println("я управляю миром! ");
-		System.out.println("привет мир!");
-	}
-}
