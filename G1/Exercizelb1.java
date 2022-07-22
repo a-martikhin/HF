1cdf7582becddc04a87db7fb2b25eb2c4267c577
a.martikhin <a.martikhin@mail.ru>
2022-07-23 01:12:26 +0300

    Навел порядок в папках

---
+++ G1/Exercizelb1.java
@@ -0,0 +1,12 @@
+public class Exercizelb1{
+	public static void main(String[] args){
+		int x = 5;
+		while(x < 10){
+			
+			if(x > 3){
+				System.out.println("bolshoi iks");
+			}
+			x = x-1;
+		}
+	}
+}
---
+++ G1/MyFirstApp.java
@@ -0,0 +1,6 @@
+public class MyFirstApp{
+	public static void main(String[] args){
+		System.out.println("я управляю миром! ");
+		System.out.println("привет мир!");
+	}
+}
