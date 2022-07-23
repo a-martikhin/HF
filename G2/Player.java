5f55c1378542cc2dc824ecb23db1274c7fbf3073
a.martikhin <a.martikhin@mail.ru>
2022-07-24 00:24:07 +0300

    Последняя задачка главы

--- G2/Player.java
+++ G2/Player.java
@@ -3,6 +3,6 @@ public class Player{
 	
 	public void guess() {
 	    number = (int) (Math.random() * 10);
-		System.out.println("Думаю, это число " + number)
+		System.out.println("Думаю, это число " + number);
 	}
 }
