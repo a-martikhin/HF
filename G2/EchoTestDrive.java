b67be87e15573300fbe519a8aa7fe72fa6d271be
a.martikhin <a.martikhin@mail.ru>
2022-08-02 23:54:53 +0300

    Финальный эхо тест-драйв =)

--- G2/EchoTestDrive.java
+++ G2/EchoTestDrive.java
@@ -6,10 +6,10 @@ class EchoTestDrive{
         while(x < 4){
             e1.hello();
             e1.count = e1.count + 1;
-            if(x == 3) {
+            if(x < 4) {
                 e2.count = e2.count + 1;
             }
-            if(x > 0){
+            if(x < 4){
                 e2.count = e2.count + e1.count;
             }
             x = x + 1;
