4df59c9eb7ab9c839e1d85706c771cf309f50519
a.martikhin <a.martikhin@mail.ru>
2022-08-14 00:17:02 +0300

    Продолжаю решение задач из главы 4

---
+++ G4/Puzzle4.java
@@ -0,0 +1,23 @@
+class Puzzle4{
+    public static void main(String[] args){
+        Puzzle4b[] obs = new Puzzle4b[6];
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while(x < 6){
+            obs[x] = new Puzzle4b();
+            
+        }
+    }
+}
+
+class Puzzle4b{
+    int ivar;
+    public int doStuff(int factor){
+        if(ivar > 100){
+            return ;
+        } else {
+            return ;
+        }
+    }
+}
