a0367af261526b277c11051b4102996106914fa7
Aleksandr Martikhin <a.martikhin@mail.ru>
2023-05-20 00:53:02 +0300

    Приступил к изучению главы 9

---
+++ G9/Duck.java
@@ -0,0 +1,9 @@
+class Duck{
+    int size;
+    public Duck(int DuckSize){
+        System.out.println("Кря");
+        System.out.println("Значение по умолчанию равно " + size);
+        size = DuckSize;
+        System.out.println("Значение после инициализации " + size);
+    }
+}
